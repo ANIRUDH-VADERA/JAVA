3c5743c398256da2f92d3e41aa69b252e47c75d2
ANIRUDH-VADERA <anirudhvadera@gmail.com>
2023-04-04 13:47:20 +0530

    Added changes

---
+++ Activity2/Pattern1/pattern1.java
@@ -0,0 +1,13 @@
+package activity2;
+
+class pattenr1 {
+    public static void main(String[] args) {
+        System.out.println("ANIRUDH VADERA(20BCE2940)");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
