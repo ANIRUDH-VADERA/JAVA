9f0d73a0b159919418096c1cacee938778b93711
ANIRUDH-VADERA <anirudhvadera@gmail.com>
2023-04-21 18:33:45 +0530

    Added string enter

---
+++ Activity3/enterString/strings1.java
@@ -0,0 +1,12 @@
+package activity3;
+
+import java.util.Scanner;
+
+class strings1 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the string :");
+        in.close();
+    }
+}
