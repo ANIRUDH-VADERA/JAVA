2c35ff695c3d7f0276156bd4fd34d2c851560ab3
ANIRUDH-VADERA <anirudhvadera@gmail.com>
2023-05-10 11:41:59 +0530

    Added super and this

---
+++ Activity4/SuperAndThis/Superthis.java
@@ -0,0 +1,21 @@
+package activity4;
+// class Parent {
+// Parent() {
+// System.out.println("I am Parent");
+// }
+// }
+
+// public class Superthis extends Parent {
+// Superthis() {
+// this(10);
+// System.out.println("I am no arg cons");
+// }
+
+// Superthis(int i) {
+// System.out.println("I am paremetried const");
+// }
+
+// public static void main(String[] args) {
+// Superthis obj = new Superthis();
+// }
+// }
